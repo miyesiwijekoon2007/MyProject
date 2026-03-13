e919ea76c231f57e1e187353b1ad611a7a17eb14
miyesiwijekoon2007 <miyesiwijekoon@gmail.com>
2026-03-13 19:46:25 +0530

    Remade server and fixed greeting app

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 const port = 3001;
@@ -21,21 +20,3 @@ app.post("/submit", (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
-=======
-//port config
-const port =3001;
-//express config
-const exp=require("express");
-//conflig server app
-const app =exp();
-const name = "Chamudini Thathsarani";
-const batch = "Batch 2024";
-
-app.listen(port, () => {
-    console.log(`Server started at Port: ${port}`);
-    console.log(`Name: ${name}`);
-    console.log(`Batch: ${batch}`);
-    console.log("Press Ctrl + C to stop the server");
-});
-// app.listen(port);
->>>>>>> 4f31b1d931e1c9b4526a4d34d0b4669848c9a70d
